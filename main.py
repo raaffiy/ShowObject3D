b4a0a2757cd9247f3222c3e453c6acbd77f747f5
raaffiy <raafimaulana43@gmail.com>
2024-06-24 11:59:29 +0700

    update object 3d

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class SoftwareRender:
     def create_objects(self):
         self.camera = Camera(self, [-5, 6, -55])
         self.projection = Projection(self)
-        self.object = self.get_object_from_file('resources/craneo.obj')
+        self.object = self.get_object_from_file('resources/head.obj')
         self.object.rotate_y(-math.pi / 4)
 
     def get_object_from_file(self, filename):
